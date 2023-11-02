2098f1192cde1ea895da583e7d116872e5377a80
douglasrolins <douglasrolins@gmail.com>
2023-11-01 21:55:24 -0300

    Subir aplicação no github

---
+++ app/model/Servico.php
@@ -0,0 +1,25 @@
+<?php
+
+
+class  Servico
+{
+    private $nome;
+    private $tempo;
+    private $valor;
+    private $logo;
+
+    // Método para cadastrar um serviço
+    public function cadastrar()
+    {
+    }
+
+    // Método para atualizar um serviço
+    public function atualizar()
+    {
+    }
+
+    // Método para apagar um serviço
+    public function apagar()
+    {
+    }
+}
